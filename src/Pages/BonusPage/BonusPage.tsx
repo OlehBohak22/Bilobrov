291febedaadd4daf985658bdab62f771144c4dbc
Oleh <eddrfgthyjn@gmail.com>
2025-05-05 12:00:28 +0300

    .

--- src/Pages/BonusPage/BonusPage.tsx
+++ src/Pages/BonusPage/BonusPage.tsx
@@ -2,11 +2,11 @@ import { BonusFaq } from "../../components/BonusFaq/BonusFaq";
 import { BonusPageHero } from "../../components/BonusPageHero/BonusPageHero";
 import { BonusSection } from "../../components/BonusSection/BonusSection";
 import { BonusSystem } from "../../components/BonusSystem/BonusSystem";
-// import s from "./BonusPage.module.css";
+import s from "./BonusPage.module.css";
 
 export const BonusPage = () => {
   return (
-    <main>
+    <main className={s.page}>
       <BonusPageHero />
       <BonusSection />
       <BonusSystem />
